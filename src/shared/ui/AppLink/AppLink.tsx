568e3cbf9654299167c3298053727ba318745c89
kotovvach <kotovvach@gmail.com>
2025-02-14 18:24:20 +0700

    add babel, add extract plugin

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,21 @@
+import {Link, LinkProps} from "react-router-dom";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './AppLink.module.scss';
+import {FC} from "react";
+
+interface AppLinkProps extends LinkProps {
+    className?: string
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const { to, className, children, ...otherProps } = props;
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, {}, [className])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+};
