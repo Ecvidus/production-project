30a469665fe93186ba6c74a2db9eb74da16d9f00
kotovvach <kotovvach@gmail.com>
2025-03-17 18:35:52 +0700

    add jest, add first test

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-    console.log('RANDOM fdsfsadfsdsdf 1243254234545');
-
-    return 'fff';
-}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar)}>
-        <div className={cls.links}>
-            <Link to="/" className={cls.mainLink}>Главная</Link>
-            <Link to="/about">О сайте</Link>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation('navbar');
+    return (
+        <div className={classNames(cls.Navbar)}>
+            <div className={cls.links}>
+                <Link to="/" className={cls.mainLink}>
+                    {t('Главная')}
+                </Link>
+                <Link to="/about">
+                    {t('О сайте')}
+                </Link>
+            </div>
         </div>
-    </div>
-);
+    );
+};
