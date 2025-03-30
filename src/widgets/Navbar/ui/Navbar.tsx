2388c7ebcb4bf981fae93b6a9852476b27babef0
kotovvach <kotovvach@gmail.com>
2025-03-30 17:51:14 +0700

    add main pipeline github actions

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,11 +4,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
-interface NavbarProps {
-    className?: string
-}
-
-export const Navbar = ({ className }: NavbarProps) => {
+export const Navbar = () => {
     const { t } = useTranslation('navbar');
     return (
         <div className={classNames(cls.Navbar)}>
