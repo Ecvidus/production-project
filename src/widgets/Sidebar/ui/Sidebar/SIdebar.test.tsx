06250676e6b3efab07357a65d6fcc8f97a93fae4
kotovvach <kotovvach@gmail.com>
2025-04-01 14:03:56 +0700

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/SIdebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/SIdebar.test.tsx
@@ -2,16 +2,17 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import React from 'react';
 import { withTranslation } from 'react-i18next';
-import { renderWithTranslation } from 'shared/lib/renderWithTranlation/renderWithTranlation';
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTranlation/renderWithTranlation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('classNames', () => {
     test('with only first param', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
